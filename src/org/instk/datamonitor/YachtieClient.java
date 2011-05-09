2c44dce3d8015507046fef5d3a90ba50d9d2fe26
Scott <Scott@scotts.(none)>
2011-05-09 18:59:43 +0800

    Retry connection

--- src/org/instk/datamonitor/YachtieClient.java
+++ src/org/instk/datamonitor/YachtieClient.java
@@ -9,25 +9,40 @@ public class YachtieClient {
 	
 	private Socket sendingSocket;
 	private OutputStreamWriter socketStream;
-	private boolean isOpen;
+	
+	private String server;
+	private int port;
 	
 	private long lastHeadingTime = 0;
 	private long lastGPSTime = 0;
-	
+	private long lastConnectTime = 0;
+		
 	private long updateThreshold = 2000;
+	private long connectThreshold = 5000;
 	
 	public YachtieClient(String server, int port) {
-		isOpen = false;
-		try {
-			sendingSocket = new Socket(server,8081);
-			socketStream = new OutputStreamWriter(sendingSocket.getOutputStream());
-			isOpen = true;
-		} catch (IOException e) {
-			System.out.println("Failed opening socket: " + e);
+		this.server = server;
+		this.port = port;
+		tryConnect();
+	}
+
+	private void tryConnect() {
+		long now = new Date().getTime();
+		if (now - lastConnectTime > connectThreshold) {
+			lastConnectTime = now;	// Do this first, because we want to wait even for failures.
+			try {
+				sendingSocket = new Socket(server,port);
+				socketStream = new OutputStreamWriter(sendingSocket.getOutputStream());
+			} catch (IOException e) {
+				System.out.println("Failed opening socket: " + e);
+			}
 		}
 	}
 	
 	public void recordGPS(double lat, double lng, double alt, double accuracy) {
+		if (!isOpen())
+			tryConnect();
+		
 		long now = new Date().getTime();
 		if (now - lastGPSTime > updateThreshold) {
 			streamLog(String.format("GPS,%f,%f,%f,%f\n", 
@@ -40,6 +55,9 @@ public class YachtieClient {
 	}
 	
 	public void recordHeading(float heading) {
+		if (!isOpen())
+			tryConnect();
+		
 		long now = new Date().getTime();
 		if (now - lastHeadingTime > updateThreshold) {
 			streamLog(String.format("3,%f", heading));
@@ -48,7 +66,7 @@ public class YachtieClient {
 	}
 	
 	public boolean isOpen() {
-		return isOpen;
+		return sendingSocket.isConnected();
 	}
 	
 	private void streamLog(String message)
